05bad8ad887b7337a17f0ab742672c5ffc554edf
Panna <pavanksshetty@gmail.com>
2019-08-13 11:27:28 -0700

    Updates

---
+++ precache-manifest.f595687719d7a01d3561ee065e0a4a1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b7fadea1330bac8905ba9ca771ca13",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6ee1fd7fb827a530facd",
+    "url": "/my-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "f23d7df1b6b60c90880b",
+    "url": "/my-portfolio/static/css/main.5c46c5b8.chunk.css"
+  },
+  {
+    "revision": "6ee1fd7fb827a530facd",
+    "url": "/my-portfolio/static/js/2.77f3d42b.chunk.js"
+  },
+  {
+    "revision": "f23d7df1b6b60c90880b",
+    "url": "/my-portfolio/static/js/main.ed128872.chunk.js"
+  },
+  {
+    "revision": "d30fa1a6b67316ef599f",
+    "url": "/my-portfolio/static/js/runtime~main.8e6d56fe.js"
+  },
+  {
+    "revision": "7834a0cd33bf75b1e14ecf86524e8bf9",
+    "url": "/my-portfolio/static/media/profile.7834a0cd.jpg"
+  }
+]);
